d8f932b78300bcca894d7e9f4a27ad6023cfd0b7
Justin Megahan <justinrmegahan@gmail.com>
2015-03-12 18:02:01 -0500

    High School Data
    Added Standardised testing, ACT, AP, SAT, etc

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'http://rubygems.org'
 
 gem "middleman", "~>3.1.6"
 
+gem 'middleman-imageoptim'
+
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
 
--- config.rb
+++ config.rb
@@ -100,11 +100,9 @@ end
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_css
+  activate :minify_javascript
+  activate :imageoptim
 
   # Enable cache buster
   # activate :asset_hash
@@ -114,4 +112,5 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
 end
